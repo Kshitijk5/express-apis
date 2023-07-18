c042271f57be1ed4241c5596a4b83cb3f234cd95
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-07-18 13:04:23 +0530

    misc updates

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import express from "express";
+import morgan from "morgan";
+import { userController } from "./controllers/userController.js";
+import dotenv from "dotenv";
 
 export const app = express();
+
+//config
+dotenv.config({ path: `./config.env` });
+app.use(morgan("dev"));
+app.use(express.json());
+
+//routes
+// user route
+app.use("/users", userController);
